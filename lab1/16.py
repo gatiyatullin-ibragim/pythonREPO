6747685bc1616798e80d5549729989bd47dcea81
ibragim <hxxx81538@gmail.com>
2025-01-21 08:24:33 +0500

    escape character

---
+++ lab1/16.py
@@ -0,0 +1,16 @@
+"""
+Escape Character
+To insert characters that are illegal in a string, use an escape character.
+
+An escape character is a backslash \ followed by the character you want to insert.
+
+An example of an illegal character is a double quote inside a string that is surrounded by double quotes: 
+"""
+
+#error example:
+#txt = "We are the so-called "Vikings" from the north."
+
+
+#correct example:
+txt = "We are the so-called \"Vikings\" from the north."
+
