0a0af40318dc3fdf381f127bfae46adb5d618a55
ibragim <hxxx81538@gmail.com>
2025-01-21 11:03:32 +0500

    operators

---
+++ lab2/operators1.py
@@ -0,0 +1,72 @@
+#Python groups of opearators
+"""
+Arithmetic operators
+Assignment operators
+Comparison operators
+Logical operators
+Identity operators
+Membership operators
+Bitwise operators
+"""
+"""
++	Addition	x + y	
+-	Subtraction	x - y	
+*	Multiplication	x * y	
+/	Division	x / y	
+%	Modulus	x % y	
+**	Exponentiation	x ** y	
+//	Floor division	x // y
+"""
+
+"""
+=   	x = 5   	x = 5	
++=	    x += 3	    x = x + 3	
+-=	    x -= 3	    x = x - 3	
+*=	    x *= 3   	x = x * 3	
+/=	    x /= 3	    x = x / 3	
+%=	    x %= 3	    x = x % 3	
+//=	    x //= 3  	x = x // 3	
+**=	    x **= 3 	x = x ** 3	
+&=	    x &= 3  	x = x & 3	
+|=	    x |= 3  	x = x | 3	
+^=	    x ^= 3  	x = x ^ 3	
+>>=	    x >>= 3   	x = x >> 3	
+<<=	    x <<= 3  	x = x << 3	
+:=	    print(x := 3)   	x = 3 print(x)
+
+"""
+
+""" 
+==	Equal	x == y	
+!=	Not equal	x != y	
+>	Greater than	x > y	
+<	Less than	x < y	
+>=	Greater than or equal to	x >= y	
+<=	Less than or equal to	x <= y
+"""
+
+
+"""
+and 	Returns True if both statements are true	x < 5 and  x < 10	
+or	Returns True if one of the statements is true	x < 5 or x < 4	
+not	Reverse the result, returns False if the result is true	not(x < 5 and x < 10)
+"""
+
+"""
+() 	Parentheses	
+**	 Exponentiation	
++x  -x  ~x  	Unary plus, unary minus, and bitwise NOT	
+*  /  //  %	Multiplication, division, floor division, and modulus	
++  -	Addition and subtraction	
+<<  >>	Bitwise left and right shifts	
+&	Bitwise AND	
+^	Bitwise XOR	
+|	Bitwise OR		
+not	 Logical NOT	
+and 	AND	
+or 	OR
+"""
+
+#example
+print((6 + 3) - (6 + 3))
+
