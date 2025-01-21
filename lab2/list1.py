4e727e5f44ac206d43caa2e2bd19f39804a87c2b
ibragim <hxxx81538@gmail.com>
2025-01-21 11:11:40 +0500

    lists

---
+++ lab2/list1.py
@@ -0,0 +1,35 @@
+#to make list we its name and then write members of list inside brackets []
+
+#example:
+mylist = ["banana","apple","pineaple"]
+print(mylist)
+
+#lists are allow us to keep dublicate members
+mylist = ["banana","apple","pineaple","banana"]
+print(mylist)
+
+
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+#example of string, int and bool datatypes
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+#also lists can contain several datatypes
+list1 = ["abc", 34, True, 40, "male"]
+
+
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+
+"""
+Python Collections (Arrays)
+There are four collection data types in the Python programming language:
+
+List is a collection which is ordered and changeable. Allows duplicate members.
+Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+Dictionary is a collection which is ordered** and changeable. No duplicate members.
+"""
