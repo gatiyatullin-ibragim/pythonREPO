947c52c356ce13c0c474d08c80e54c29a453fefc
ibragim <hxxx81538@gmail.com>
2025-01-21 08:49:22 +0500

    false values

---
+++ lab2/bool4.py
@@ -0,0 +1,11 @@
+#what are false values?
+
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+
+#it might be an false value or none value like 0, empty string, empty list, empty set, empty tuple and etc.
