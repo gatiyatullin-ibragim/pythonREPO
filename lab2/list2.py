f3b015692d7fef3adf8b515f843364b72f410545
ibragim <hxxx81538@gmail.com>
2025-01-22 18:07:17 +0500

    items access

---
+++ lab2/list2.py
@@ -0,0 +1,14 @@
+#Python - Access List Items
+#List items are indexed and you can access them by referring to the index number:
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
+
+
+#Print the last item of the list:
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
+
+
+#Return the third, fourth, and fifth item:
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
