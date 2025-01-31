7ef930153160ae9d2784181bb5a7b4344e915a78
ibragim <hxxx81538@gmail.com>
2025-01-31 10:39:12 +0500

    ex2

---
+++ lab3/classes_objects/ex3.py
@@ -0,0 +1,10 @@
+from turtle import Shape
+
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
