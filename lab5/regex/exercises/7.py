76de1132bbb9e57c8ca63f303b594082e70c4040
ibragim <hxxx81538@gmail.com>
2025-02-14 09:15:22 +0500

    ex7

--- lab5/regex/exercises/7.py
+++ lab5/regex/exercises/7.py
@@ -1,6 +1,6 @@
 def snake_to_camel(str):
     words = str.split('_')
-    camel_str = words[0] + ''.join(word.capitalize() for word in wrds[1:])
+    camel_str = words[0] + ''.join(word.capitalize() for word in words[1:])
     return camel_str
 
 snake_case_str = str(input())
